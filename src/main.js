c6e81f08c1a5993ad12b74262f663e6975044c18
Reshat <reshat.sultan@yandex.ru>
2021-11-06 11:28:53 +0300

    Revert "Issue 2"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import { createApp } from 'vue';
-import Antd from 'ant-design-vue';
-import App from './App';
-import 'ant-design-vue/dist/antd.css';
-import router from './router'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).use(router).use(Antd).mount('#app')
+createApp(App).mount('#app')
